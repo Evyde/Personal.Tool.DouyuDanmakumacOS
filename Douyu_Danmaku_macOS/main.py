605caad1588caa1fa33357a4db1466d508577c78
ForeverOpp <life.app.hanfeng@hotmail.com>
2020-09-08 00:08:46 +0800

    fix special thanks.

--- Douyu_Danmaku_macOS/main.py
+++ Douyu_Danmaku_macOS/main.py
@@ -22,9 +22,16 @@ async def main():
     await dmc.start()
 i18n.set('available_locales', ['en_US', 'zh_CN', 'en'])
 i18n.set('locale', 'en')
-i18n.set('fallback', "en_US")
+i18n.set('fallback', "en")
+i18n.set('file_format', 'yaml')
+i18n.config.json_available = False
+i18n.config.yaml_available = True
+i18n.add_translation('foo.h', 'bar', locale='en_US')
+i18n.add_translation('foo.h', '傻逼', locale='zh_CN')
 
-i18n.load_path.append("Douyu_Danmaku_macOS/i18n")
-print(i18n.t('en.title.a', locale='en_US'))
+i18n.load_path.append("C:\\Users\\Evyde\\PycharmProjects\\Douyu_Danmu_For_macOS\\Douyu_Danmaku_macOS")
+print(i18n.t('title.a', locale='en'))
+print(i18n.config.settings)
+print(i18n.t('foo.h', locale='zh_CN'))
 
-asyncio.run(main())
+#asyncio.run(main())
--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 发现macOS上面确实没什么好的斗鱼弹幕查看软件，先撸一个只有消息和礼物提醒的东西出来。
 
 # 感谢
-- [littlecodersh/danmu](https://github.com/littlecodersh/danmu)
+- [IsoaSFlus/danmaku](https://github.com/IsoaSFlus/danmaku)
 - [jaredks/rumps](https://github.com/jaredks/rumps)
 - [ChengHaoMou](https://github.com/ChenghaoMou/touchbar-lyric)
   
